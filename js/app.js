0b454eb0b93cbba12abca9d0f9d875431b037c79
shrikant21 <55912627+shrikant21@users.noreply.github.com>
2020-08-03 09:48:06 +0530

    Added My CV

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#profile__ripple").ripples({
+        resolution:512,
+        dropRadius:10
+    })
+})
+
+$(document).ready(function(){
+    $("#profile__ripple1").ripples({
+        resolution:512,
+        dropRadius:10
+    })
+})
+$(document).ready(function(){
+    $("#profile__ripple3").ripples({
+        resolution:512,
+        dropRadius:10
+    })
+})
+
+$(document).ready(function(){
+    $("#profile__ripple2").ripples({
+        resolution:512,
+        dropRadius:10
+    })
+
+});
+
+$('.slider').slick({
+    
+});
